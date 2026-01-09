a94be56f0420e60b5202d9893d8c2c4eb2fa74bb
swetatop <dan.0985850201@gmail.com>
2026-01-08 22:35:28 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
